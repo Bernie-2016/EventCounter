c2b952101dff53ae52264c63fbeafdb57149d88a
Alex Coventry <coventry@gmail.com>
2015-11-05 19:59:49 -0500

    Fix BSD interface to get ALL events

--- bsd.py
+++ bsd.py
@@ -1,12 +1,66 @@
-import os, json
+import os, json, datetime, httplib, time, cPickle, re
+from dateutil.parser import parse as parse_date
 from bsdapi.BsdApi import Factory
+from . import config
 
-api = Factory().create(
-    id=os.environ['BSDID'], secret=os.environ['BSDSECRET'],
-    host=os.environ['BSDHOST'], port=os.environ.get('BSDPORT',None) or '80',
-    securePort=os.environ.get('BSDSECUREPORT', None) or '443')
+api = Factory().create(id=config.bsdid, secret=config.bsdsecret,
+                       host=config.bsdhost, port=80, securePort=443)
 
-def get_events_since(date):
-    resp = api.doRequest('/event/search_events', {'create_date_start': date},
-                         api.GET, None)
-    return json.loads(resp.body)
+api.verbose=True
+
+date_format = '%Y-%m-%d %H:%M:%S'
+
+def get_search_results(date):
+    req = dict(date_start=str(date), limit='10000')
+    for dummy in 10*[None]:
+        resp = api.doRequest('/event/search_events', req , api.GET, None)
+        # Sometimes  you  get a  temporary  'bad  gateway' from  this.
+        # retry a few times.
+        if not (resp.http_status == 502 and resp.http_reason == 'Bad Gateway'):
+            break
+            time.sleep(5)
+    if resp.http_status != 200:
+        raise httplib.HTTPException, 'Event search failed:\n' + str(resp)
+    return resp
+
+def get_dates(events, date_field='start_dt', default='1972-08-02 06:00:00'):
+    nonexistent = "The event_id_obfuscated '[a-z0-9]{,10}' does not exist in the system."
+    return [datetime.datetime.strptime(e.get(date_field, default), date_format)
+            for e in events if not re.match(nonexistent, str(e))]
+
+never = datetime.datetime(2500, 1, 1)
+
+def process_events(resp):
+    events = json.loads(resp.body)
+    events = [e for e in events if e['name'] != 'This is merely a test.']
+    dates = get_dates(events)
+    assert dates == sorted(dates), 'Events ordered by start_dt'
+    by_id = dict((e['event_id_obfuscated'], e) for e in events)
+    return events, dates[-1] if dates else never, by_id
+
+def get_events_since(since):
+    # Need to do this with start_dt, not create_dt.  Some events don't
+    # have  a create_dt!   Also,  pagination is  automatically set  to
+    # 2500.  It can handle 10,000 at a time, but there are more events
+    # than that.  So need to page through them.
+    if 'EVENTCOUNTERCURRENTEVENTS' in os.environ:
+        # This  assumes  that  EVENTCOUNTERCURRENTEVENTS points  to  a
+        # pickle file with ALL the events up to the given date
+        events = cPickle.load(open(os.environ['EVENTCOUNTERCURRENTEVENTS']))
+        since = max(parse_date(since), max(get_dates(events, 'create_dt')))
+    resp = get_search_results(str(since))
+    events, latest, ids = process_events(resp)
+    while True:
+        resp = get_search_results(latest)
+        cevents, newlatest, cids = process_events(resp)
+        overlap = set(cids).intersection(set(ids))
+        if len(cids) == len(overlap): # Must be no more to get
+            break
+        events.extend([e for e in cevents
+                       if e['event_id_obfuscated'] not in overlap])
+        ids.update(cids)
+        latest = newlatest
+    return events
+
+# t = get_events('Jan 1 1970')
+# print len(t)
