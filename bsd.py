1796bbebda2862a77a798e6e6a5f97dbeef1143c
Alex Coventry <coventry@gmail.com>
2015-11-07 01:26:17 -0500

    Set bigger limit on bsd search request results

--- bsd.py
+++ bsd.py
@@ -11,7 +11,11 @@ api.verbose=True
 date_format = '%Y-%m-%d %H:%M:%S'
 
 def get_search_results(date):
-    req = dict(date_start=str(date), limit='1000')
+    # The higher you set this limit, the greater the risk of a network
+    # timeout/failure, but the lower you  set it, the greater the risk
+    # of too  many events on one  day causing the pagination  to fail.
+    # (But that would be a nice problem to have!)
+    req = dict(date_start=str(date), limit='13000')
     for dummy in 10*[None]:
         resp = api.doRequest('/event/search_events', req , api.GET, None)
         # Sometimes  you  get a  temporary  'bad  gateway' from  this.
@@ -61,6 +65,11 @@ def get_events_since(since):
         events.extend([e for e in cevents
                        if e['event_id_obfuscated'] not in overlap])
         ids.update(cids)
+        # If you don't request enough  events, the trick of paginating
+        # by the setting date_start to the latest event in the current
+        # batch will fail,  because you'll get the same  set of events
+        # on two successive requests, and this code will stop.
+        assert len(set(e['start_dt'] for e in events)) > 1, 'May need to set the request limit higher'
         latest = newlatest
     return events
 
